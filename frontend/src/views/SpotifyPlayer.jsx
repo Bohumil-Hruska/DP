23688d258b07be39a41504c91dc98d320ef215fe
Bohumil Hruška <bohumil.hruska@tul.cz>
2025-05-05 11:16:13 +0200

    Spotify

--- frontend/src/views/SpotifyPlayer.jsx
+++ frontend/src/views/SpotifyPlayer.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import SpotifyEmbed from './SpotifyEmbed';
+
 
 const SpotifyPlayer = ({ showMessage }) => {
     const [devices, setDevices] = useState([]);
